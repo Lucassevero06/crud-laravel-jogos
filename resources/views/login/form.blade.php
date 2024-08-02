aaea514775e4e49a0780d2f53cba85494c0f3ad1
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-08-02 14:08:45 -0400

    Update form.blade.php

--- resources/views/login/form.blade.php
+++ resources/views/login/form.blade.php
@@ -9,23 +9,23 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <title>Olá Mundo!</title>
 </head>
-<body class="container-fluid bg-dark text-light">
+<body class="container-fluid">
 
 <div class="d-flex flex-column flex-md-row justify-content-center align-items-center min-vh-100 w-100">
-    <div class="w-50">
+    <div class="w-50 mx-auto text-center">
         <h1>Olá Mundo!</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto commodi culpa ducimus, est ex facilis fuga fugiat illum ipsum nemo officia perferendis praesentium quaerat quasi quo reiciendis reprehenderit, suscipit tempora?</p>
     </div>
     <div class="w-50">
-        <form class="border border-light p-3 rounded d-flex flex-column w-50 mx-auto">
+        <form class="shadow-lg p-3 mb-5 bg-body-tertiary p-3 rounded d-flex flex-column w-50 mx-auto">
             <div class="row mb-3 w-100 mx-auto">
                 <h1 class="text-center">Salve papito</h1>
             </div>
             <div class="row mb-3 w-100 mx-auto">
-                <input type="email" class="form-control" id="inputEmail3">
+                <input placeholder="example@example.com" type="email" class="form-control " id="inputEmail3">
             </div>
             <div class="row mb-3 w-100 mx-auto">
-                <input type="password" class="form-control" id="inputPassword3">
+                <input placeholder="1234" type="password" class="form-control" id="inputPassword3">
             </div>
             <button type="submit" class="btn btn-primary w-100 mx-auto">Sign in</button>
         </form>
